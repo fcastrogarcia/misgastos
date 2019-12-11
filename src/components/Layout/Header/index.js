0685fe862db884549a000796dedce3d5b18e2697
fcastrogarcia <fede.castrog@gmail.com>
2019-12-10 21:13:37 -0300

    Refile header component

--- src/components/Layout/Header/index.js
+++ src/components/Layout/Header/index.js
@@ -3,13 +3,11 @@ import styles from "./Header.module.scss";
 import { MdAttachMoney } from "react-icons/md";
 import Menu from "./Menu/index";
 
-const Header = () => {
-  return (
-    <header>
-      <MdAttachMoney className={styles.logo} />
-      <Menu />
-    </header>
-  );
-};
+const Header = () => (
+  <header>
+    <MdAttachMoney className={styles.logo} />
+    <Menu />
+  </header>
+);
 
 export default Header;
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,7 +1,11 @@
 import React from "react";
 import styles from "./Layout.module.scss";
 import cx from "classnames";
+import Header from "./Header/index";
 
 export default ({ children }) => (
-  <div className={cx(styles.layout)}>{children}</div>
+  <div className={cx(styles.layout)}>
+    <Header />
+    {children}
+  </div>
 );
